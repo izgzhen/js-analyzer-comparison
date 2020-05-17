7607678526c2395f4eee66a17ae8276bfb950a75
Zhen Zhang <git@zhen-zhang.com>
2020-05-16 18:56:01 -0700

    Multi-process

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from msbase.subprocess_ import try_call_std
-from msbase.utils import log_progress
+from msbase.subprocess_ import try_call_std, multiprocess
+from msbase.utils import log_progress, write_pretty_json
+
+import random
 import glob
+import time
+import os
 
 js_files = glob.glob("test262-master/test/**/*.js", recursive=True)
 print("Total JS files: %s" % len(js_files))
@@ -20,12 +24,32 @@ tools = {
     "safe": safe_cmd
 }
 
-for f in log_progress(js_files, print_item=True):
+NPROC = 4
+if os.getenv("NPROC") is not None:
+    NPROC = int(os.getenv("NPROC")) # type: ignore
+
+random.seed(0)
+
+tasks = random.sample(js_files, k=20)
+
+def process(f: str):
+    result = {}
     for name, get_cmd in tools.items():
+        now = time.time()
         stdout, stderr, code = try_call_std(get_cmd(f), print_cmd=False, output=False, noexception=True)
+        duration_seconds = time.time() - now
         if code == 0:
-            print("[%s] Success" % name)
+            result[name] = {
+                "status": "ok",
+                "duration_seconds": duration_seconds
+            }
         else:
-            print("[%s] Failure" % name)
-            print(stdout)
-            print(stderr)
+            result[name] = {
+                "status": "failure",
+                "duration_seconds": duration_seconds
+            }
+    return result
+
+results = multiprocess(process, tasks, return_dict=False, n=NPROC, throws=True)
+
+write_pretty_json(results, "results.json")
