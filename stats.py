5e7fa7ce51251897299048a6e8f71c041d3b3713
Zhen Zhang <git@zhen-zhang.com>
2020-05-17 09:09:35 -0700

    Track shared count

--- stats.py
+++ stats.py
@@ -8,13 +8,24 @@ print("Total: %s" % total)
 
 counts = {}
 
+shared_counts = {}
+
 for result in results:
-    for name, info in result.items():
+    tools = list(result["outputs"].keys())
+    tool_pairs = [ (tools[i], tools[j]) for i in range(0, len(tools) - 1) for j in range(i + 1, len(tools))]
+    for name, info in result["outputs"].items():
         if name not in counts:
             counts[name] = {
                 "ok": 0,
                 "failure": 0
             }
         counts[name][info["status"]] += 1
+    for pair in tool_pairs:
+        if pair not in shared_counts:
+            shared_counts[pair] = 0
+        t1, t2 = pair
+        if result["outputs"][t1]["status"] == result["outputs"][t2]["status"]:
+            shared_counts[pair] += 1
 
 print(counts)
+print(shared_counts)
